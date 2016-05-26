033f8f09529e5c49bcb8414ae3cc7d0845bfe4ed
Aleksey Sofronov <asofronov@issart.com>
2016-05-26 14:30:21 +0600

    Добавлен исполняемый класс Distance

---
+++ sandbox/src/main/java/Distance.java
@@ -0,0 +1,13 @@
+public class Distance {
+    public static double distance(Point p1, Point p2) {
+        return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
+    }
+
+
+    public static void main(String[] args) {
+        Point p1 = new Point(2, 4);
+        Point p2 = new Point(8, 9);
+        System.out.println("Расстояние между двумя точками равно " + distance(p1, p2));
+
+    }
+}
