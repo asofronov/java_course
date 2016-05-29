df1eac71dec49ff2d1f84f2159158b1fd0eccb77
Aleksey Sofronov <asofronov@issart.com>
2016-05-29 17:44:42 +0600

    Корректировки по методу. Исправление ошибок

--- sandbox/src/main/java/jc/sas/points/Distance.java
+++ sandbox/src/main/java/jc/sas/points/Distance.java
@@ -6,7 +6,7 @@ public class Distance {
     public static void main(String[] args) {
         Point p1 = new Point(1, 5);
         Point p2 = new Point(4, 3);
-        System.out.println("Расстояние между двумя точками равно " + p1.distance(p1, p2));
+        System.out.println("Расстояние между двумя точками равно " + p1.distance(p2));
 
     }
 }
--- sandbox/src/test/java/jc/sas/points/PointsTest.java
+++ sandbox/src/test/java/jc/sas/points/PointsTest.java
@@ -9,10 +9,10 @@ public class PointsTest {
 
     @Test
     public void testPoint() {
-        Point p1 = new Point(3,10);
-        Point p2 = new Point(4,20);
-        Assert.assertEquals(p1.distance(p1,p2),10.04987562112089);
-        assert p1.distance(p1,p2) == 5;
+        Point p1 = new Point(3, 10);
+        Point p2 = new Point(4, 20);
+        Assert.assertEquals(p1.distance(p2),10.04987562112089);
+
     }
 
 }
