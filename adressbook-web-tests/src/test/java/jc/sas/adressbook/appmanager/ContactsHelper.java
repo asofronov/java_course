ac5f14f065200cac1e037a87ef9d2fcf4bb6bda8
Aleksey Sofronov <asofronov@issart.com>
2016-06-05 21:17:23 +0600

    Создан ContactsHelper.java

---
+++ adressbook-web-tests/src/test/java/jc/sas/adressbook/appmanager/ContactsHelper.java
@@ -0,0 +1,94 @@
+package jc.sas.adressbook.appmanager;
+
+import jc.sas.adressbook.model.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by aleks on 05.06.2016.
+ */
+public class ContactsHelper {
+    private FirefoxDriver wd;
+
+    public ContactsHelper(FirefoxDriver wd) {
+        this.wd = wd;
+    }
+
+    public void submitContactCreation() {
+        wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+    }
+
+    public void fillOtherData(OtherData otherData) {
+        wd.findElement(By.name("homepage")).click();
+        wd.findElement(By.name("homepage")).clear();
+        wd.findElement(By.name("homepage")).sendKeys(otherData.getWebsite());
+        wd.findElement(By.name("address2")).click();
+        wd.findElement(By.name("address2")).clear();
+        wd.findElement(By.name("address2")).sendKeys(otherData.getAddress2());;
+        wd.findElement(By.name("phone2")).click();
+        wd.findElement(By.name("phone2")).clear();
+        wd.findElement(By.name("phone2")).sendKeys(otherData.getHomePhone2());
+        wd.findElement(By.name("notes")).click();
+        wd.findElement(By.name("notes")).clear();
+        wd.findElement(By.name("notes")).sendKeys(otherData.getNoteText());
+    }
+
+    public void fillEmailsData(EmailsData emailsData) {
+        wd.findElement(By.name("email")).click();
+        wd.findElement(By.name("email")).clear();
+        wd.findElement(By.name("email")).sendKeys(emailsData.getEmail1());
+        wd.findElement(By.name("email2")).click();
+        wd.findElement(By.name("email2")).clear();
+        wd.findElement(By.name("email2")).sendKeys(emailsData.getEmail2());
+        wd.findElement(By.name("email3")).click();
+        wd.findElement(By.name("email3")).clear();
+        wd.findElement(By.name("email3")).sendKeys(emailsData.getEmail3());
+    }
+
+    public void fillPhonesData(PhonesData phonesData) {
+        wd.findElement(By.name("home")).click();
+        wd.findElement(By.name("home")).clear();
+        wd.findElement(By.name("home")).sendKeys(phonesData.getHomePhone1());
+        wd.findElement(By.name("mobile")).click();
+        wd.findElement(By.name("mobile")).clear();
+        wd.findElement(By.name("mobile")).sendKeys(phonesData.getMobilePhone());
+        wd.findElement(By.name("work")).click();
+        wd.findElement(By.name("work")).clear();
+        wd.findElement(By.name("work")).sendKeys(phonesData.getWorkPhone());
+        wd.findElement(By.name("fax")).click();
+        wd.findElement(By.name("fax")).clear();
+        wd.findElement(By.name("fax")).sendKeys(phonesData.getFax());
+    }
+
+    public void fillBusinessData(BusinessData businessData) {
+        wd.findElement(By.name("title")).click();
+        wd.findElement(By.name("title")).clear();
+        wd.findElement(By.name("title")).sendKeys(businessData.getTitle());
+        wd.findElement(By.name("company")).click();
+        wd.findElement(By.name("company")).clear();
+        wd.findElement(By.name("company")).sendKeys(businessData.getCompany());
+        wd.findElement(By.name("address")).click();
+        wd.findElement(By.name("address")).clear();
+        wd.findElement(By.name("address")).sendKeys(businessData.getAddress1());
+    }
+
+    public void fillNamesData(NamesData namesData) {
+        wd.findElement(By.name("firstname")).click();
+        wd.findElement(By.name("firstname")).clear();
+        wd.findElement(By.name("firstname")).sendKeys(namesData.getFirstName());
+        wd.findElement(By.name("middlename")).click();
+        wd.findElement(By.name("middlename")).clear();
+        wd.findElement(By.name("middlename")).sendKeys(namesData.getMidName());
+        wd.findElement(By.name("lastname")).click();
+        wd.findElement(By.name("lastname")).clear();
+        wd.findElement(By.name("lastname")).sendKeys(namesData.getLastName());
+        wd.findElement(By.name("nickname")).click();
+        wd.findElement(By.name("nickname")).clear();
+        wd.findElement(By.name("nickname")).sendKeys(namesData.getNickname());
+
+    }
+
+    public void addButton() {
+        wd.findElement(By.linkText("add new")).click();
+    }
+}
--- adressbook-web-tests/src/test/java/jc/sas/adressbook/tests/ContactCreationTests.java
+++ adressbook-web-tests/src/test/java/jc/sas/adressbook/tests/ContactCreationTests.java
@@ -7,13 +7,13 @@ public class ContactCreationTests extends TestBase {
 
     @Test
     public void ContactCreationTests() {
-        app.addButton();
-        app.fillNamesData(new NamesData("First", "Mid", "Last", "Nickname"));
-        app.fillBusinessData(new BusinessData("Address1", "Company", "Title"));
-        app.fillPhonesData(new PhonesData("123456", "1234567", "12345678", "123456789"));
-        app.fillEmailsData(new EmailsData("test@test.ru", "test2@test.ru", "test3@test.ru"));
-        app.fillOtherData(new OtherData("http://ya.ru", "Address2", "Home", "Note"));
-        app.submitContactCreation();
+        app.getContactsHelper().addButton();
+        app.getContactsHelper().fillNamesData(new NamesData("First", "Mid", "Last", "Nickname"));
+        app.getContactsHelper().fillBusinessData(new BusinessData("Address1", "Company", "Title"));
+        app.getContactsHelper().fillPhonesData(new PhonesData("123456", "1234567", "12345678", "123456789"));
+        app.getContactsHelper().fillEmailsData(new EmailsData("test@test.ru", "test2@test.ru", "test3@test.ru"));
+        app.getContactsHelper().fillOtherData(new OtherData("http://ya.ru", "Address2", "Home", "Note"));
+        app.getContactsHelper().submitContactCreation();
         app.backHomePage();
     }
 
