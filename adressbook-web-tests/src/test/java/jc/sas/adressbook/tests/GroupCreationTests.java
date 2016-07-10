106029b181afe587090af117959d7d6220b30cb8
Aleksey Sofronov <asofronov@issart.com>
2016-07-10 14:04:23 +0600

    driver updated and hamcrest libraries added

--- adressbook-web-tests/src/test/java/jc/sas/adressbook/tests/GroupCreationTests.java
+++ adressbook-web-tests/src/test/java/jc/sas/adressbook/tests/GroupCreationTests.java
@@ -1,10 +1,16 @@
 package jc.sas.adressbook.tests;
 
 import jc.sas.adressbook.model.GroupData;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.*;
+import static org.testng.Assert.*;
 
 public class GroupCreationTests extends TestBase {
 
@@ -15,11 +21,12 @@ public class GroupCreationTests extends TestBase {
         GroupData group = new GroupData().withName("test1").withHeader("test").withFooter("test3");
         app.group().create(group);
         Set<GroupData> after = app.group().all();
-        Assert.assertEquals(after.size(), before.size() + 1);
+        assertThat(after.size(), equalTo(before.size() + 1));
 
         group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt());
         before.add(group);
-        Assert.assertEquals(before, after);
+        assertThat(after, equalTo(before));
+
     }
 
 
