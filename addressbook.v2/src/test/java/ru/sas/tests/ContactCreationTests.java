7d1f78ed0e6375a4c31112fca71cce67057ce0be
Aleksey Sofronov <asofronov@issart.com>
2016-07-24 22:04:26 +0600

    l4_m9

--- addressbook.v2/src/test/java/ru/sas/tests/ContactCreationTests.java
+++ addressbook.v2/src/test/java/ru/sas/tests/ContactCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.sas.model.ContactData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -21,14 +22,8 @@ public class ContactCreationTests extends TestBase {
         List<ContactData> after = app.getContactHelper().getContactList();
         Assert.assertEquals(after.size(), before.size() + 1);
 
+        contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(contact);
-        int max = 0;
-        for (ContactData c : after) {
-            if (c.getId() > max) {
-                max = c.getId();
-            }
-        }
-        contact.setId(max);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 
