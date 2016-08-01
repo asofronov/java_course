3a96d08e50ee1695585c36fe2df8975a3bb74ffa
Aleksey Sofronov <asofronov@issart.com>
2016-08-01 22:48:22 +0600

    l7_m6

--- addressbook.v2/src/test/java/ru/sas/model/ContactData.java
+++ addressbook.v2/src/test/java/ru/sas/model/ContactData.java
@@ -7,6 +7,9 @@ import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
 import java.io.File;
+import java.util.HashSet;
+import java.util.Set;
+
 @XStreamAlias("contact")
 @Entity
 @Table (name = "addressbook")
@@ -51,6 +54,14 @@ public class ContactData {
     private String allDetails;
     @Transient
     private File photo;
+
+    @ManyToMany (fetch = FetchType.EAGER)
+    @JoinTable(name = "address_in_groups", joinColumns = @JoinColumn(name = "id"), inverseJoinColumns = @JoinColumn(name = "group_id"))
+    private Set<GroupData> groups = new HashSet<GroupData>();
+
+    public Set<GroupData> getGroups() {
+        return new Groups(groups);
+    }
 
     public ContactData withId(int id) {
         this.id = id;
